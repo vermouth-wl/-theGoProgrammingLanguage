eaedc21373aa724978386b116f6c387369cb35c4
苦艾酒 <330025157@qq.com>
2021-07-14 11:46:09 +0800

    39、增加使用普通函数构建一个协程

---
+++ src/goroutine/goroutine1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 使用普通函数构建一个协程
+func running1() {
+
+	var times int
+
+	// 构建一个无限循环，times每隔1秒+1
+	for {
+		times++
+		fmt.Println("协程1", times)
+
+		// 延时1秒
+		time.Sleep(time.Second)
+	}
+}
+
+// 使用普通函数构建一个协程
+func running2() {
+
+	var times int
+
+	// 构建一个无限循环，times每隔1秒+1
+	for {
+		times++
+		fmt.Println("协程2", times)
+
+		// 延时1秒
+		time.Sleep(time.Second)
+	}
+}
+
+// main
+func main() {
+
+	// 并发执行程序
+	go running1()
+	go running2()
+
+	// 接受命令行输入，不做任何处理
+	var input string
+	fmt.Scan(&input)
+
+}
