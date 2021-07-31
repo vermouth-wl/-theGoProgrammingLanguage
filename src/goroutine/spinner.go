e62dd743e3a7089bc78f07b0dbe09fd4b59791a7
苦艾酒 <330025157@qq.com>
2021-07-31 14:39:56 +0800

    50、创建计算第N个斐波那契数列，计算期间使用spinner进行可见性提示

---
+++ src/goroutine/spinner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fix(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fix(x-1) + fix(x-2)
+}
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fix(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
